091c98d70365c4f43ed4af00d3d0bcb78f2bc70a
itafm <fatimaezahra.msik@gmail.com>
2023-03-29 13:17:12 +0000

    change

--- src/App.js
+++ src/App.js
@@ -4,16 +4,13 @@ import AddTask from "./pages/AddTask";
 import { Home } from "./pages/Home";
 
 topbar.config({
-  autoRun: false,
-  barThickness: 5,
+  
   barColors: {
-    0: "rgba(26,  188, 156, .7)",
-    0.3: "rgba(41,  128, 185, .7)",
-    1.0: "rgba(231, 76,  60,  .7)",
+    0: "rgb(229, 11, 11)",
+    0.3: "rgb(229, 11, 11)",
+    1.0: "rgb(229, 11, 11)",
   },
-  shadowBlur: 5,
-  shadowColor: "red",
-  className: "topbar",
+  
 });
 
 function App() {
